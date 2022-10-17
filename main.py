6a694be80160cacacbf8d077b0b6b77f9a300f98
GithubMastor <seoreilan90@gmail.com>
2022-10-17 18:51:14 -0500

    finish typing-speed-tester program

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import csv
 import random
 import time
+from PIL import Image, ImageTk, ImageDraw, ImageFont
 
 ############  SENTENCE GETTER  ############
 def sentencer():
@@ -14,7 +15,7 @@ def sentencer():
 ############  REGISTER PRESS  ############
 active = True
 def onKeyPress(event):
-    if (event.char.isalnum() or event.char.isspace()) and active==True:
+    if (event.char.isalnum() or event.char.isspace() or event.char =='-' or event.char ==','or event.char =="'") and active==True:
         key_checker(event.char,sent)
 
 ############  CHECK PRESS  ############
@@ -28,15 +29,28 @@ def onKeyPress(event):
 #     def getval(self):
 #         return self.counter
 
+############  RESET  ############
+def reset():
+    global cnt, active, start, end, charpersec, wordpermin, incorrect, sent
+    sent = sentencer().lower()
+    my_font1 = ('times', 30, 'bold')
 
+    label1.config(text=('TYPE SENTENCE BELOW:\n\n' + ''))
+    text.configure(state='normal')
+    text.delete("1.0", "end")
+    text.insert(tk.END,chars= sent)
+    text.configure(state='disabled')
+    label2.config(text=('Char/Sec:                  \nWord/Min:                  \n% Correct:                  '),  justify='left')
+
+    cnt,charpersec,wordpermin,incorrect=0,0,0,0
+    active=True
 
 
 ############  CHECK PRESS  ############
 cnt = 0
+incorrect = 0
 def key_checker(char,sentence):
-    global cnt, active, start, end, charpersec, wordpermin
-
-    print(char + '\n' +  sentence[cnt] + '\n' + sentence)
+    global cnt, active, start, end, charpersec, wordpermin, incorrect
     if cnt == (len(sentence)-2):
         if char == sentence[cnt]:
             text.configure(state='normal')
@@ -44,21 +58,25 @@ def key_checker(char,sentence):
             text.tag_add("o", f"1.{cnt}", )
             text.tag_configure("o", foreground='green')
             text.configure(state='disabled')
-            text.pack()
+            text.grid(row=1, column=0, columnspan=1)
+            # text.pack()
         else:
             text.configure(state='normal')
             text.tag_remove('cur', f"1.{cnt}")
             text.tag_add("x", f"1.{cnt}")
             text.tag_configure("x", foreground='red')
             text.configure(state='disabled')
-            text.pack()
+            text.grid(row=1, column=0, columnspan=1)
+            # text.pack()
         active=False
         end = time.time()
         timed = end - start
         print(timed)
         charpersec = round((len(sent) / timed),2)
         wordpermin = round((charpersec*12),2)
-        print(f'Characters Per Second: {charpersec}\nWords Per Minute (average): {wordpermin}')
+        percor = round(((len(sent)-incorrect)/len(sent))*100,2)
+        print(f'Characters Per Second: {charpersec}\nWords Per Minute (average): {wordpermin}\n% Correct: {percor}%')
+        label2.config(text=(f'Char/Sec:   {charpersec}\nWord/Min: {wordpermin}\n% Correct: {percor}%'))
     elif char == sentence[cnt]:
         if cnt==0:
             start = time.time()
@@ -70,7 +88,8 @@ def key_checker(char,sentence):
         text.tag_add("cur",f"1.{cnt+1}",)
         text.tag_configure("cur",background='orange')
         text.configure(state='disabled')
-        text.pack()
+        text.grid(row=1, column=0, columnspan=1)
+        # text.pack()
     else:
         text.configure(state='normal')
         text.tag_remove('cur',f"1.{cnt}")
@@ -79,39 +98,45 @@ def key_checker(char,sentence):
         text.tag_add("cur",f"1.{cnt+1}",)
         text.tag_configure("cur",background='orange')
         text.configure(state='disabled')
-        text.pack()
+        text.grid(row=1, column=0, columnspan=1)
+        # text.pack()
+        incorrect+=1
+
     cnt+=1
 
 
 
 
 ############  WINDOW  ############
+
 sent = sentencer().lower()
 window = tk.Tk()
 sw = round(window.winfo_screenwidth() / 2)
-sh = round(window.winfo_screenheight() / 2)
+sh = round(window.winfo_screenheight() / 3)
 window.geometry(f'{sw}x{sh}')
 window.configure(bg='#FFFFFF')
 my_font1=('times', 30, 'bold')
 
 label1 = tk.Label(window,text=('TYPE SENTENCE BELOW:\n\n' + ''),width=30,font=my_font1,bg='#FFFFFF')
 label1.configure(bg='#FFFFFF', wraplength=600)
-label1.grid(row=0,column=1,columnspan=6)
-label1.pack()
+label1.grid(row=0,column=0,columnspan=1)
 
-text = tk.Text(window,  font=('Sans Serif', 24), height=5, width=round(sw/40),wrap='word')
+
+text = tk.Text(window,  font=('Sans Serif', 24), height=4, width=round(sw/40),wrap='word')
 text.insert(tk.END,chars= sent)
 text.configure(state='disabled')
-text.pack()
+text.grid(row=1,column=0,columnspan=1)
 
-# label2 = tk.Label(window,text=('TYPE SENTENCE BELOW:\n\n' + ''),width=15,font=my_font1,bg='#FFFFFF')
-# label2.configure(bg='#FFFFFF', wraplength=600)
-# label2.grid(row=2,column=1,columnspan=3)
-# label2.pack()
 
-# text2 = tk.Text(window,  font=('Sans Serif', 24), height=5, width=round(sw/40),wrap='word')
-# text2.configure(state='disabled')
-# text2.pack()
+label2 = tk.Label(window,text=('Char/Sec:                  \nWord/Min:                  \n% Correct:                  '),width=30,font=my_font1,bg='#FFFFFF', justify='left')
+label2.configure(bg='#FFFFFF', wraplength=400)
+label2.grid(row=1,column=1,columnspan=1)
+
+button1 = tk.Button(window, text='Browse Files',width=20,command = lambda:reset())
+btn_img = Image.open('button_reset.png')
+imz = ImageTk.PhotoImage(btn_img)
+button1.configure(image=imz, width=imz.width(), bg='#FFFFFF',borderwidth=0)
+button1.grid(row=2,column=0,columnspan=1, padx=10, pady=10)
 
 ############  TIMER  ############
 start = time.time()
